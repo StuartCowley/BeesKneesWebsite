ff4ccf27f4c5d21bdeeb4b85d3cb33ac9b3d1d04
StuartCowley <voidstalker@live.co.uk>
2024-09-22 13:44:52 +0100

    Installed sass, updated eslint config, add rudimentary header and footer, renamed css files to scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+import Header from "./components/Header/Header";
 import localFont from "next/font/local";
-import "./globals.css";
+import "./globals.scss";
+import Footer from "./components/Footer/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
