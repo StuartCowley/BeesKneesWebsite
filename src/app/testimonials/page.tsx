92e716b08e663d879027378c60cb5e7c9d817b5b
StuartCowley <voidstalker@live.co.uk>
2024-09-28 15:03:50 +0100

    Added testimonials skeleton

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.scss";
+import Footer from "../components/Footer/Footer";
+
+export default function Testimonials() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        Testimonials
+      </main>
+      <Footer />
+    </div>
+  );
+}
