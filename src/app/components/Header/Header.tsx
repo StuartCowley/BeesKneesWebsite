f42e51b807f10c6a7c548ff56611d1a8fc8eedf7
StuartCowley <voidstalker@live.co.uk>
2024-11-23 20:14:04 +0000

    Updated Header

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,27 +1,21 @@
 import Link from 'next/link';
 import Bee from '../images/Bee';
-import Facebook from "../images/Facebook"
-import Email from "../images/Email"
+import SocialBlock from '../SocialBlock/SocialBlock';
 import styles from './header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={`${styles.pageContainer} ${styles.wrap}`}>
-        <Bee className={styles.socialItem} height="48px" width="48px" />
-        <nav className={styles.nav}>
+      <div className={`${styles.pageContainer} ${styles.header__wrap}`}>
+        <div className={styles.header__iconWrap}>
+          <Bee />
+        </div>
+        <nav className={styles.header__nav}>
           <Link href={'/testimonials'}>Testimonials</Link>
           <Link href={'/song-list'}>Song List</Link>
           <Link href={'/contact'}>Contact</Link>
         </nav>
-        <div className={styles.socialItems}>
-          <div className={styles.socialItem__wrap}>
-            <Facebook className={styles.socialItem} height="24px" width="24px" />
-          </div>
-          <div className={styles.socialItem__wrap}>
-            <Email className={styles.socialItem} height="24px" width="24px" />
-          </div>
-        </div>
+        <SocialBlock />
       </div>
     </header>
   );
