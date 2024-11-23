991f721b1cd3838a19ef5197244ccb3f79663b5d
StuartCowley <voidstalker@live.co.uk>
2024-11-23 19:45:12 +0000

    Created SocialBlock component

---
+++ src/app/components/SocialBlock/SocialBlock.tsx
@@ -0,0 +1,16 @@
+import Facebook from "../images/Facebook"
+import Email from "../images/Email"
+import styles from "./socialBlock.module.scss";
+
+export default function SocialBlock() {
+  return (
+    <div className={styles.socialBlock}>
+      <div className={styles.socialBlock__itemWrap}>
+        <Facebook className={styles.socialBlock__item} height="24px" width="24px" />
+      </div>
+      <div className={styles.socialBlock__itemWrap}>
+        <Email className={styles.socialBlock__item} height="24px" width="24px" />
+      </div>
+    </div>
+  );
+}
