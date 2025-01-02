13d25551063b68876e7fd4ccc1c319c6559dbbcf
StuartCowley <voidstalker@live.co.uk>
2025-01-02 16:55:10 +0000

    Added CTA component

---
+++ src/app/components/Cta/Cta.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from 'next/link';
+import styles from './cta.module.scss';
+
+export default function Cta({ link, label } : {link: string, label: string}): React.ReactElement {
+  return (
+    <Link href={link}>
+      <div className={`${styles.cta}`}>
+        {label}
+      </div>
+    </Link>
+  );
+}
