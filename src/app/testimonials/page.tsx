9d0b4b372ddcd7dfd5d35ad46e4d4a116fd27470
StuartCowley <voidstalker@live.co.uk>
2025-01-03 11:11:42 +0000

    Added testimonials page and content

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,11 +1,22 @@
 import styles from "./page.module.scss";
+import TitleBlock from "@/app/components/TitleBlock/TitleBlock";
+import ContentWrap from "@/app/components/ContentWrap/ContentWrap";
+import testimonials from "@/app/data/testimonials.json";
 
 export default function Testimonials() {
   return (
-    <>
-      <main className={`${styles.pageContainer} ${styles.content}`}>
-        Testimonials
-      </main>
-    </>
+    <main className={`${styles.pageContainer} ${styles.content}`}>
+      <TitleBlock text="Testimonials" />
+      {testimonials.reverse().map((testimonial) => (
+        <ContentWrap key={testimonial.id}>
+          <div className={`${styles.wrap}`}>
+            <div className={`${styles.copy}`}>{testimonial.copy}</div>
+            <div className={`${styles.attribution}`}>
+              - {testimonial.author}, {testimonial.date}
+            </div>
+          </div>
+        </ContentWrap>
+      ))}
+    </main>
   );
 }
